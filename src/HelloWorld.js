be4cd54dfc5f38f7ffc0ffecd57ad078c132f7e1
iesley.santos <iesley.santos@b2wdigital.com>
2023-06-10 18:49:53 -0300

    first commit

---
+++ src/HelloWorld.js
@@ -0,0 +1,158 @@
+import React from "react";
+import { useEffect, useState } from "react";
+import {
+  helloWorldContract,
+  connectWallet,
+  updateMessage,
+  loadCurrentMessage,
+  getCurrentWalletConnected,
+} from "./util/interact.js";
+
+import alchemylogo from "./alchemylogo.svg";
+
+const HelloWorld = () => {
+  //state variables
+  const [walletAddress, setWallet] = useState("");
+  const [status, setStatus] = useState("");
+  const [message, setMessage] = useState("No connection to the network."); //default message
+  const [newMessage, setNewMessage] = useState("");
+
+  //called only once
+  useEffect(() => {
+    async function fetchMessage() {
+      const message = await loadCurrentMessage();
+      setMessage(message);
+    }
+
+    fetchMessage();
+
+    async function fetchWallet() {
+      const { address, status } = await getCurrentWalletConnected();
+      setWallet(address);
+      setStatus(status);
+    }
+    fetchWallet();
+    addWalletListener();
+  }, []);
+
+  function addSmartContractListener() { //TODO: implement
+
+  }
+
+  function addWalletListener() {
+    if (window.ethereum) {
+      window.ethereum.on("accountsChanged", (accounts) => {
+        if (accounts.length > 0) {
+          setWallet(accounts[0]);
+          setStatus("👆🏽 Write a message in the text-field above.");
+        } else {
+          setWallet("");
+          setStatus("🦊 Connect to Metamask using the top right button.");
+        }
+      });
+    } else {
+      setStatus(
+        <p>
+          {" "}
+          🦊{" "}
+          <a target="_blank" href={`https://metamask.io/download`}>
+            You must install Metamask, a virtual Ethereum wallet, in your
+            browser.
+          </a>
+        </p>
+      );
+    }
+  }
+
+  const connectWalletPressed = async () => {
+    const walletResponse = await connectWallet();
+    setStatus(walletResponse.status);
+    setWallet(walletResponse.address);
+  };
+
+  const onUpdatePressed = async () => {
+    const { status } = await updateMessage(walletAddress, newMessage);
+    setStatus(status);
+  }
+
+  //the UI of our component
+  return (
+    <div id="container">
+      <img id="logo" src={alchemylogo}></img>
+      <button id="walletButton" onClick={connectWalletPressed}>
+        {walletAddress.length > 0 ? (
+          "Connected: " +
+          String(walletAddress).substring(0, 6) +
+          "..." +
+          String(walletAddress).substring(38)
+        ) : (
+          <span>Connect Wallet</span>
+        )}
+      </button>
+
+      <h2 style={{ paddingTop: "50px" }}>Current Message:</h2>
+      <p>{message}</p>
+
+      <h2 style={{ paddingTop: "18px" }}>New Message:</h2>
+
+      <div>
+        <input
+          type="text"
+          placeholder="Update the message in your smart contract."
+          onChange={(e) => setNewMessage(e.target.value)}
+          value={newMessage}
+        />
+        <p id="status">{status}</p>
+
+        <button id="publish" onClick={onUpdatePressed}>
+          Update
+        </button>
+      </div>
+
+      <style jsx>{`
+        #container {
+          max-width: 800px;
+          margin: 0 auto;
+          padding: 20px;
+        }
+  
+        #logo {
+          max-width: 100%;
+          height: auto;
+        }
+  
+        #walletButton {
+          display: block;
+          margin: 10px auto;
+        }
+  
+        h2 {
+          margin-top: 50px;
+        }
+  
+        @media (max-width: 600px) {
+          h2 {
+            margin-top: 30px;
+          }
+  
+          #walletButton {
+            font-size: 14px;
+          }
+        }
+  
+        @media (max-width: 400px) {
+          h2 {
+            margin-top: 20px;
+          }
+  
+          #walletButton {
+            font-size: 12px;
+          }
+        }
+      `}</style>
+    </div>
+  );
+
+};
+
+export default HelloWorld;
